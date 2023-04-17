ec45f783ccdba8b0df8ff2efb0623cc6a465e880
Theresa Miles <ThereIsASmile@gmail.com>
2023-04-17 12:44:58 -0400

    second commit: fixed alignment

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -9,7 +9,6 @@ const Main = () => {
         insideOutside: ""
     })
 
-
     const changeHandler = (event) => {
         console.log(event.target.name, event.target.value)
         setPlant({...plant,[event.target.name]: event.target.value})
@@ -22,7 +21,6 @@ const Main = () => {
     const submitHandler = (event) => {
         event.preventDefault();
         setAllPlants([...allPlants, plant])
-
     }
 
 return (
@@ -32,24 +30,24 @@ return (
         <form action="" onSubmit={submitHandler} className="col-md-4 offset-2  mt-5 pt-3 pb-3 bg-success text-white rounded-3">
         <h2>Plant App</h2>
         <p>Add plants</p>
-
             <div className='form-group'>
                 <label htmlFor="plantName">Plant Name:</label>
 
                 <input type="text" name="plantName" className="form-control" onChange={changeHandler} value={plant.plantName}/>
                 {/* Don't forget to add value! */}
+                {/* changeHandler: Display in real time form data */}
                 {plant.plantName.length> 0 && plant.plantName.length < 3 ?(
                     <p>Name must be more than 3 characters.</p>
                     ) : null
                 }
-
+                {/* This is a ternary operator */}
             </div>
             <div className='form-group'>
                 
                 <label htmlFor="waterPref">Water Preference:</label>
 
                 <input type="text" name="waterPref" className="form-control" onChange={changeHandler} value={plant.waterPref}/>
-
+                {/* changeHandler: Display in real time form data */}
                 {plant.waterPref.length> 0 && plant.waterPref.length < 3 ?(
                     <p>Must be more than 3 characters.</p>
                     ) : null
@@ -60,20 +58,18 @@ return (
                 <label htmlFor="insideOutside">Indoor or outdoor plant?</label>
 
                 <input type="text" name="insideOutside" className="form-control" onChange={changeHandler} value={plant.insideOutside}/>
-
-
+                {/* changeHandler: Display in real time form data */}
             </div>
-            
             <button className='btn btn-light mt-3'>Add plant!</button>
         </form>
-        {/* Display in real time form data */}
+
         <div className='col-md-4 offset-1 mt-5 pt-3 pb-3 bg-success text-white rounded-3'>
             <h2>Display Form Data</h2>
             <p>Plant: {plant.plantName}</p>
             <p>Water preference: {plant.waterPref}</p>
             <p>Inside or outside: {plant.insideOutside}</p>
-
         </div>
+
     </div>
 </div>
 )
